821788b53008e93aed57e3603a80a51f7efaa024
Winifred Lu <winnie.lu@comcast.net>
2022-02-10 14:37:57 -0500

    small debugging fix

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 
 server.use(express.json());
-server.use('/api/users', usersRouter);
-
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
+
+server.use('/api/users', usersRouter);
 
 server.use('*', (req, res) => {
     res.send('<h1>Hello, Heroku!</h1>');
